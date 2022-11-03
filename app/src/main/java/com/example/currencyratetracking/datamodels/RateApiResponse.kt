cdaa3ab8fe97fc11c20a5ab5995ef2c9d37a4188
Roman Semin <semin.roman.i@mail.ru>
2022-11-04 01:18:59 +0300

    minor change

--- app/src/main/java/com/example/currencyratetracking/datamodels/RateApiResponse.kt
+++ app/src/main/java/com/example/currencyratetracking/datamodels/RateApiResponse.kt
@@ -1,12 +1,7 @@
 package com.example.currencyratetracking.datamodels
 
-import com.google.gson.annotations.SerializedName
-
 data class RateApiResponse(
-//    @SerializedName("rates")
     val rates: Map<String, Double>,
-//    @SerializedName("base")
     val base: String,
-//    @SerializedName("date")
     val date: String
 )
